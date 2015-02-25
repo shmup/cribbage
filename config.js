3aee9df44fbd0aa13b0065818e8abf46b1888a4c
Jared Miller <jared@bytepro.net>
2015-02-25 16:53:31 -0500

    init commit

---
+++ config.js
@@ -0,0 +1,22 @@
+System.config({
+  "baseURL": "/",
+  "paths": {
+    "*": "*.js",
+    "github:*": "jspm_packages/github/*.js",
+    "npm:*": "jspm_packages/npm/*.js"
+  }
+});
+
+System.config({
+  "map": {
+    "jquery": "github:components/jquery@2.1.3",
+    "lodash": "npm:lodash@3.3.1",
+    "github:jspm/nodelibs-process@0.1.1": {
+      "process": "npm:process@0.10.0"
+    },
+    "npm:lodash@3.3.1": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    }
+  }
+});
+
