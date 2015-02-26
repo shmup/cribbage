bbf8192ff47494362aba0d6daf987c0408ee41b7
Jared Miller <jtmiller@gmail.com>
2015-02-25 19:36:30 -0500

    more functionality

--- config.js
+++ config.js
@@ -11,10 +11,14 @@ System.config({
   "map": {
     "jquery": "github:components/jquery@2.1.3",
     "lodash": "npm:lodash@3.3.1",
+    "ramda": "npm:ramda@0.10.0",
     "github:jspm/nodelibs-process@0.1.1": {
       "process": "npm:process@0.10.0"
     },
     "npm:lodash@3.3.1": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    },
+    "npm:ramda@0.10.0": {
       "process": "github:jspm/nodelibs-process@0.1.1"
     }
   }
